23f7cab1d8abf3cb1b421dd18182686adf8ab1b2
JCM2030 <jcmiranda2030@clc.pshs.edu.ph>
2026-09-23 20:15:03 +0800

    Add files via upload

---
+++ q1_sg6_arayat_miranda (1).py
@@ -0,0 +1,14 @@
+class Lab:
+    def __init__(self,room_number):
+        self.room_number = room_number
+    
+class Technician:
+    def __init__(self,assigned_lab):
+        self.assigned_lab = ""
+    def assign_lab(self, lab_obj):
+        self.assigned_lab = lab_obj
+    
+chem_lab = Lab("302")
+mr_cruz = Technician("Mr. Cruz")
+mr_cruz.assign_lab(chem_lab)
+print(mr_cruz.assigned_lab.room_number)
